e7f27a4a4688bcd61b65ed8d2a93964597a53e24
shivam-jainn <shivam.clgstash@gmail.com>
2024-03-21 20:11:55 +0530

    feat(Backend): :tada: Backend intro
    nada
    
    nada

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,7 +1,15 @@
 import express,{ Express,Request,Response } from "express";
+
+// routes
+import { runRouter } from "./routes/coderun.routes";
+
 export const app:Express = express();
 
+app.use(express.json())
 
 app.get('/health',(req:Request,res:Response)=>{
     res.send("Server is healthy");
 });
+
+
+app.use('/run',runRouter);
